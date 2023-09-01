3a9184e9d032f16fae3736cfa33a2860aea8b3ca
sawi2003 <96415110+sawi2003@users.noreply.github.com>
2023-09-01 09:01:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,15 +8,15 @@ const fs = require('fs-extra')
  global.port= process.env.PORT || 5000 
  global.email = 'sam@secktor.live' 
  global.github = 'https://github.com/nipuna15' 
- global.location = 'Sri lanka, Monaragala' 
- global.gurl = 'https://instagram.com/' // add your username 
- global.sudo = process.env.SUDO || '919628516236' 
- global.devs = '919628516236'; 
+ global.location = 'Sri lanka, chilaw' 
+ global.gurl = 'https://instagram.com/' //umeshsubhasinghe
+ global.sudo = process.env.SUDO || '94767969374' 
+ global.devs = '94767969374'; 
  global.website = 'https://github.com/nipuna15/Blue-Lion-V2' //wa.me/+91000000000000 
  global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/nipuna15/nipuna15/main/IMG-20230613-WA0008.jpg' 
  module.exports = { 
    botname:   process.env.BOT_NAME === undefined ? 'Blue-Lion' : process.env.BOT_NAME, 
-   ownername: process.env.OWNER_NAME === undefined ? 'nipuna' : process.env.OWNER_NAME,  
+   ownername: process.env.OWNER_NAME === undefined ? 'sawiya' : process.env.OWNER_NAME,  
    author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'Nipuna' : process.env.PACK_INFO.split(";")[0], 
    auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS, 
    packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'Blue-lion' : process.env.PACK_INFO.split(";")[1],
